563baf4d77ef3cc5184d8015d099bd1afbea06d0
Nikita Simakin <tanukofisimakin@gmail.com>
2023-03-23 19:48:35 +0300

    i'm lazy..

--- codewars_test/example_2.py
+++ codewars_test/example_2.py
@@ -5,7 +5,7 @@ import codewars_test as test
 def how_much_water(water, load, clothes):
     if clothes < load:
         return 'Not enough clothes'
-    elif clothes  2*load:
+    elif clothes  2 *load:
         return 'Too much clothes'
     else:
         water_needed = water * (1.1 ** (clothes - load))
